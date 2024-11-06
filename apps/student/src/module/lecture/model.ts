6174e39df0e81e170d83c34996c28a531aa4511b
arky02 <kyean07@naver.com>
2024-11-06 16:28:41 +0900

    fix: lecture model 할당 error

--- apps/student/src/module/lecture/model.ts
+++ apps/student/src/module/lecture/model.ts
@@ -1,34 +1,35 @@
 import { type Prisma } from '@core/models'
 
 export const getLectureInfoInclude = {
-	_count: true,
-	analyzedLecture: {
-		include: {
-			segments: {
-				orderBy: {
-					timeStamp: 'asc'
-				},
-				include: {
-					textWithTimestamps: {
-						orderBy: {
-							timeStamp: 'asc'
-						}
-					},
-					frames: true,
-					framesId: true
-				}
-			},
-			questions: true,
-			fullSummarization: true,
-			thumbnailsId: true
-		}
-	},
-	academyMembers: {
-		include: {
-			user: true
-		}
-	}
-} satisfies Prisma.LectureInclude
+	_count: true, 
+  analyzedLecture: {
+    select: {
+      segments: {
+        orderBy: { timeStamp: 'asc' },
+        select: {
+          id: true,
+          title: true,
+          timeStamp: true,
+          summarization: true,
+          framesId: true,
+          textWithTimestamps: {
+            orderBy: { timeStamp: 'asc' },
+            select: { id: true, timeStamp: true,segmentId: true, text: true },
+          },
+          frames: true, // frames 관계형 데이터 포함
+        },
+      },
+      questions: true,
+      fullSummarization: true,
+      thumbnailsId: true,
+    },
+  },
+  academyMembers: {
+    include: {
+      user: true,  // academyMembers 관계 데이터 중 user 정보 포함
+    },
+  },
+} satisfies Prisma.LectureInclude;
 export type GetLectureInfo = Prisma.LectureGetPayload<{
-	include: typeof getLectureInfoInclude
-}>
+  include: typeof getLectureInfoInclude;
+}>;
