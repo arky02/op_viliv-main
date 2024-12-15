e1b863b73bdadd19f34cb7e15e20831ee9ad8fdb
arky02 <kyean07@naver.com>
2024-12-15 18:23:48 +0900

    fix: serverside component rendering error

--- apps/academy/src/app/academy/[academyId]/student/[classId]/lecture/[lectureId]/detail/page.tsx
+++ apps/academy/src/app/academy/[academyId]/student/[classId]/lecture/[lectureId]/detail/page.tsx
@@ -1,4 +1,8 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import { lectureService } from '@/module/lecture/service'
+import { type GetLectureInfo } from '@/module/lecture/model'
 import { LectureDetailHeader } from './_ui/lecture-detail-header'
 import { LectureDetailArea } from './_ui/lecture-detail-area'
 
@@ -10,17 +14,32 @@ interface LectureDetailPageProps {
 	}
 }
 
-export default async function LectureDetailPage({
+export default function LectureDetailPage({
 	params
 }: LectureDetailPageProps) {
-	const lecture = await lectureService.getLectureInfo(
-		params.lectureId
-	)
+	const [lecture, setLecture] = useState<GetLectureInfo>()
+
+	useEffect(() => {
+		const getLectureInfo = async () => {
+			const lectureVal = await lectureService.getLectureInfo(
+				params.lectureId
+			)
+			setLecture(lectureVal)
+		}
+		void getLectureInfo()
+	}, [params.lectureId])
 
 	return (
 		<div className="max-pc:bg-background">
-			<LectureDetailHeader lecture={lecture} params={params} />
-			<LectureDetailArea lecture={lecture} params={params} />
+			{lecture ? (
+				<>
+					<LectureDetailHeader
+						lecture={lecture}
+						params={params}
+					/>
+					<LectureDetailArea lecture={lecture} params={params} />
+				</>
+			) : null}
 		</div>
 	)
 }
--- apps/student/src/app/academy/[classId]/lecture/[lectureId]/detail/page.tsx
+++ apps/student/src/app/academy/[classId]/lecture/[lectureId]/detail/page.tsx
@@ -1,4 +1,8 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import { lectureService } from '@/module/lecture/service'
+import { type GetLectureInfo } from '@/module/lecture/model'
 import { LectureDetailHeader } from '../../../_ui/lecture-detail-header'
 import { LectureDetailArea } from '../../../_ui/lecture-detail-area'
 
@@ -8,18 +12,32 @@ interface LectureDetailPageProps {
 	}
 }
 
-export default async function LectureDetailPage({
+export default function LectureDetailPage({
 	params
 }: LectureDetailPageProps) {
-	const { lectureId } = params
+	const [lecture, setLecture] = useState<GetLectureInfo>()
 
-	const lecture =
-		await lectureService.getLectureInfo(lectureId)
+	useEffect(() => {
+		const getLectureInfo = async () => {
+			const lectureVal = await lectureService.getLectureInfo(
+				params.lectureId
+			)
+			setLecture(lectureVal)
+		}
+		void getLectureInfo()
+	}, [params.lectureId])
 
 	return (
 		<div className="max-pc:bg-background">
-			<LectureDetailHeader lecture={lecture} params={params} />
-			<LectureDetailArea lecture={lecture} />
+			{lecture ? (
+				<>
+					<LectureDetailHeader
+						lecture={lecture}
+						params={params}
+					/>
+					<LectureDetailArea lecture={lecture} />
+				</>
+			) : null}
 		</div>
 	)
 }
