370b09594f181965b350f1ab05b1f702fe5079bd
arky02 <kyean07@naver.com>
2025-01-02 01:28:39 +0900

    feat: 기기 정보 및 변경 사유 fetch 및 update DB 액션 구현 (device1,2,3)

--- apps/academy/src/module/user/service.ts
+++ apps/academy/src/module/user/service.ts
@@ -77,6 +77,63 @@ class UserService {
 
 		return updatedUser
 	}
+
+
+	// 기기 정보 업데이트
+	async updateMobileDeviceInfo(userId: string, deviceName: string) {
+		const updatedUser = await db.user.update({
+			where: { id: userId },
+			data: {
+				device1: deviceName
+			}
+		})
+
+		return String(updatedUser?.device1 ?? '')
+	}
+	async updateTabletDeviceInfo(userId: string, deviceName: string) {
+		const updatedUser = await db.user.update({
+			where: { id: userId },
+			data: {
+				device2: deviceName
+			}
+		})
+
+		return String(updatedUser?.device2 ?? '')
+	}
+	async updatePcDeviceInfo(userId: string, deviceName: string) {
+		const updatedUser = await db.user.update({
+			where: { id: userId },
+			data: {
+				device3: deviceName
+			}
+		})
+
+		return String(updatedUser?.device3 ?? '')
+	}
+
+	// 기기 변경 사유 가져오기 및 업데이트
+	async getDeviceChangeReasonByUserId(id: string) {
+		const user = await db.user.findUnique({
+			where: {
+				id
+			}
+		})
+		return String(user?.device_change_reason ?? '')
+	}
+
+
+	async updateDeviceChangeReason(userId: string, reason: string) {
+		const updatedUser = await db.user.update({
+			where: { id: userId },
+			data: {
+				device_change_reason: reason
+			}
+		})
+
+		return String(updatedUser?.device_change_reason ?? '')
+	}
+
 }
 
 export const userService = new UserService()
+
